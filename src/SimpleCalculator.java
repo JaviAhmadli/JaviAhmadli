5deab11963261e357d6e8cc3de5727b65f591e4a
JaviAhmadli <cavidan.ahmadli@gmail.com>
2023-12-06 04:31:25 +0300

    1

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,32 @@
+public class SimpleCalculator {
+    double firstNumber;
+    double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+    public double getAdditionResult(){
+        return firstNumber+secondNumber;
+    }
+    public double getSubtractionResult(){
+        return firstNumber-secondNumber;
+    }
+    public static void main(int a, int b){
+
+    }
+    SimpleCalculator a=new SimpleCalculator();
+
+
+}
