f67f879ce2c938a87af8ea8f6c52b68b510ee429
JaviAhmadli <cavidan.ahmadli@gmail.com>
2023-12-15 03:54:16 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[]args){
+        System.out.println("main");
         var a= Transporter.getTransporter();
         System.out.println(a);
         var b= Transporter.getTransporter();
